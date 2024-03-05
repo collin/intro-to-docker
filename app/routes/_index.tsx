1dad04ead61f036e4f42783c89134336c51bfe91
Collin Miller <collintmiller@gmail.com>
2024-03-05 06:57:28 -0600

    redirect index to modules

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type MetaFunction, LoaderFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
+};
+
+export const loader: LoaderFunction = () => {
+  return redirect("/modules");
 };
 
 export default function Index() {
